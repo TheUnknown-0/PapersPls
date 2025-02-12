83be3d0940474e4a33bb6a74d48a9fb0b13192bf
Moritz Lingens <minecraft-mo@posteo.de>
2025-02-12 15:25:27 +0100

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+import game.*;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        start start = new start();
+        start.on_start();
+    }
+}
